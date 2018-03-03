7a1289d9bd9d61773ed6eb21b625ffab6df82a78
Taichi Uchino <uchino.taichi@gmail.com>
2018-03-04 02:00:20 +0900

    add navbar

--- app/assets/javascripts/books.coffee
+++ app/assets/javascripts/books.coffee
@@ -1,3 +1,13 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on 'turbolinks:load', ->
+	$('.book-simple').click(toggle_detail)
+
+toggle_detail = ->
+	$(this).children(".book-detail").toggle()
+	return
+
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180203152610) do
+ActiveRecord::Schema.define(version: 20180303151148) do
 
   create_table "books", force: :cascade do |t|
     t.string "isbn"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20180203152610) do
     t.boolean "dl"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "body"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
   end
 
 end
